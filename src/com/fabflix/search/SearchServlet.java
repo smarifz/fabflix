11446e6434117c44868f5a86f540875b62b2f94c
Arif <arif@nomis.com>
2017-02-02 21:34:48 -0800

    movielist corrected.

--- src/com/fabflix/search/SearchServlet.java
+++ src/com/fabflix/search/SearchServlet.java
@@ -3,6 +3,8 @@ package com.fabflix.search; /**
  */
 // Import required java libraries
 
+import com.fabflix.beans.Movie;
+import com.fabflix.movielist.MovielistDAO;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "SearchServlet", urlPatterns = {"/api/search"})
@@ -26,8 +29,10 @@ public class SearchServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
         Gson gson = new Gson();
         SearchDAO s = new SearchDAO();
+        MovielistDAO m = new MovielistDAO();
         String advSearch = request.getParameter("advSearch");
-        List movies = null;
+        ArrayList<Movie> movies = new ArrayList<>();
+        List movies_allData = new ArrayList<>();
 
         if(advSearch.equals("true")){
             String title = request.getParameter("title");
@@ -36,14 +41,21 @@ public class SearchServlet extends HttpServlet {
             String fname = request.getParameter("fname");
             String lname = request.getParameter("lname");
 
-            movies= s.executeSearch(true,title, year, director, fname, lname);
+            movies= (ArrayList<Movie>) s.executeSearch(true,title, year, director, fname, lname);
+
         }else{
             String searchParam = request.getParameter("searchParams");
-            movies= s.executeSearch(false,searchParam, searchParam, searchParam, searchParam, searchParam);
+            movies= (ArrayList<Movie>) s.executeSearch(false,searchParam, searchParam, searchParam, searchParam, searchParam);
+
         }
 
-        String movies_json = gson.toJson(movies);
+        for(int i = 0; i < movies.size(); i++){
+            movies_allData.add(m.getMovie(movies.get(i).getId()));
+        }
+        String movies_json = gson.toJson(movies_allData);
         writer.print(movies_json);
+
+
     }
 
 
